a6fbb4d45f4286afd5ed54afd209118d9e4df64e
Julia Hanard <julia.hanard@rea-group.com>
2022-09-09 11:41:51 +1000

    populated data

--- src/data.js
+++ src/data.js
@@ -1,7 +1,172 @@
 const playlistData = [
     {
         id: 1,
-        playlistName: "",
+        playlistName: "Ambient Lounge I",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 2,
+        playlistName: "Ambient Lounge II",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 3,
+        playlistName: "Best of Boards Of Canada",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 4,
+        playlistName: "Good Night",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 5,
+        playlistName: "Best of Moby",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 6,
+        playlistName: "IDM",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 7,
+        playlistName: "Best of Aphex Twin",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 7,
+        playlistName: "Soundtrack",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 7,
+        playlistName: "Ambient Chillout",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 7,
+        playlistName: "Lounge Cafe",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 7,
+        playlistName: "Best of Eitan Reiter",
+        genre: [],
+        description: "",
+        totalTime: "",
+        photo: "",
+        musics: [
+            {
+                artist: "",
+                title: "",
+                time: "",
+            }
+        ],
+    },
+    {
+        id: 7,
+        playlistName: "Relax",
         genre: [],
         description: "",
         totalTime: "",
