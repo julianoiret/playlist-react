2d29cef6e5111c3e658de80f488065ef21396d2a
Julia Hanard <julia.hanard@rea-group.com>
2022-11-10 15:52:16 +1100

    carousel test file

--- src/carousel/Carousel.test.js
+++ src/carousel/Carousel.test.js
@@ -1,28 +1,42 @@
-// import { render, screen } from '@testing-library/react';
-// import Carousel from './carousel';
+/* eslint-disable testing-library/await-async-query */
+/* eslint-disable testing-library/prefer-screen-queries */
+import { render } from '@testing-library/react';
+import Carousel from './carousel';
 
-// describe("Playlist card", () => {
-//     const mockPlaylistData = {
-//         id: "0",
-//         playlistName: 'Best Of Moby',
-//         genre: ['electro', 'downtempo', 'ambient', 'techno'],
-//         description: 'One of the most important electronic dance music figures of the 90s. With more than 20 albums released, it wasn t easy to choose. This playlist deserves much more tracks!',
-//         totalTime: '49.77 minutes',
-//         youtube: 'https://www.youtube.com/watch?v=QhZnEagfjTQ&list=PLGcMNmVTewK-SxM3q1HtZFyJm29In7Xjq',
-//         musics: [
-//             {
-//                 track: 1,
-//                 artist: 'Moby',
-//                 title: 'Porcelain',
-//                 time: 4.06,
-//                 link: 'https://www.youtube.com/watch?v=QhZnEagfjTQ', 
-//             },
-//         ],
-//     }
+describe("Playlist card", () => {
+    const mockPlaylistData = [
+        {
+        id: 1,
+        playlistName: 'Best Of Moby',
+        genre: ['electro', 'downtempo', 'ambient', 'techno'],
+        description: 'playlist description',
+        totalTime: '49.77 minutes',
+        youtube: 'https://www.youtube.com/watch?v=QhZnEagfjTQ&list=PLGcMNmVTewK-SxM3q1HtZFyJm29In7Xjq',
+        musics: [
+            {
+                track: 1,
+                artist: 'Moby',
+                title: 'Porcelain',
+                time: 4.06,
+                link: 'https://www.youtube.com/watch?v=QhZnEagfjTQ', 
+            },
+        ],
+    }
+    ];
    
-//     it("should render playlist title", () => {
-//         const { getByText } = render(<Carousel data={mockPlaylistData} />); 
-//         expect(screen.getByText('Best Of Moby')).toBeInTheDocument();
-//     })
-// });
+    it("should render playlist title", () => {
+        const { getAllByText } = render(<Carousel data={mockPlaylistData} />);
+        expect(getAllByText("Best Of Moby")).toBeTruthy();
+    });
+
+    it("should render playlist description", () => {
+        const { getAllByText } = render(<Carousel data={mockPlaylistData} />);
+        expect(getAllByText("playlist description")).toBeTruthy();
+    });
+
+    it("should display detail button", () => {
+        const { getByRole } = render(<Carousel data={mockPlaylistData} />);
+        expect(getByRole("button", { name: /detail/i })).toBeInTheDocument();
+    });
+});
 
